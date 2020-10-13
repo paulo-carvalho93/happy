d4f4133bb2f18a12e3569ab9067b6dbf94040ade
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-10-13 20:29:09 -0300

    Adding Controller and Routes for GET/POST methods

---
+++ backend/src/database/migrations/1602631681182-create_images.ts
@@ -0,0 +1,10 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createImages1602631681182 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+}
