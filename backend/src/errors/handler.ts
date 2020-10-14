dd07538d000c16fce2ca7bb7c841f480e7592e8e
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-10-13 21:38:56 -0300

    Adding validation and Cors configuration

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,21 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+interface ValidationsErros {
+  [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+  if (error instanceof ValidationError) {
+    let errors: ValidationsErros = {};
+
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+
+    return response.status(400).json({ messagge: 'Validation fails', errors });
+  }
+  
   console.log(error);
 
   return response.status(500).json({ message: 'Internal server error' });
