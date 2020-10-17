1b3c2aac6cfa5a726df5899afd53dcf924c1a75d
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-10-17 09:53:22 -0300

    feat (mobile): including function to select Images during form save

--- mobile/src/pages/CreateOrphanage/OrphanageData.tsx
+++ mobile/src/pages/CreateOrphanage/OrphanageData.tsx
@@ -1,56 +1,129 @@
-import React from 'react';
-import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
+import { useRoute } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+
+interface OrphanageDataRouteParams {
+  position: {
+    latitude: number;
+    longitude: number;
+  }
+}
 
 export default function OrphanageData() {
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekends, setOpenOnWeekends] = useState(true);
+  const [images, setImages] = useState<string[]>([]);
+
+  const route = useRoute();
+  const params = route.params as OrphanageDataRouteParams;
+
+  function handleCreateOrphanage() {
+    const { latitude, longitude } = params.position;
+
+    console.log({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    });
+  }
+
+  async function handleSelectImages() {
+    const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+    
+    if (status !== 'granted') {
+      alert('Attetion! We need access to select your photos!');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+
+    if (result.cancelled) {
+      return;
+    }
+
+    const { uri: image } = result;
+
+    setImages([...images, image]);
+  }
+  
+
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
-      <Text style={styles.title}>Dados</Text>
+      <Text style={styles.title}>Data</Text>
 
-      <Text style={styles.label}>Nome</Text>
+      <Text style={styles.label}>Name</Text>
       <TextInput
         style={styles.input}
+        value={name}
+        onChangeText={setName}
       />
 
-      <Text style={styles.label}>Sobre</Text>
+      <Text style={styles.label}>About</Text>
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={about}
+        onChangeText={setAbout}
       />
 
-      <Text style={styles.label}>Whatsapp</Text>
-      <TextInput
-        style={styles.input}
-      />
-
-      <Text style={styles.label}>Fotos</Text>
-      <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
+      <Text style={styles.label}>Photos</Text>
+      <View style={styles.uploadImagesContainer}>
+        {images.map(image => {
+          return (
+            <Image
+              key={image}
+              source={{ uri: image }}
+              style={styles.uploadImage}
+            />
+          );
+        })}
+      </View>
+      <TouchableOpacity style={styles.imagesInput} onPress={handleSelectImages}>
         <Feather name="plus" size={24} color="#15B6D6" />
       </TouchableOpacity>
 
       <Text style={styles.title}>Visitação</Text>
 
-      <Text style={styles.label}>Instruções</Text>
+      <Text style={styles.label}>Instructions</Text>
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={instructions}
+        onChangeText={setInstructions}
       />
 
-      <Text style={styles.label}>Horario de visitas</Text>
+      <Text style={styles.label}>Visiting hours</Text>
       <TextInput
         style={styles.input}
+        value={opening_hours}
+        onChangeText={setOpeningHours}
       />
 
       <View style={styles.switchContainer}>
-        <Text style={styles.label}>Atende final de semana?</Text>
+        <Text style={styles.label}>Opens at the weekend?</Text>
         <Switch 
           thumbColor="#fff" 
           trackColor={{ false: '#ccc', true: '#39CC83' }}
+          value={open_on_weekends}
+          onValueChange={setOpenOnWeekends}
         />
       </View>
 
-      <RectButton style={styles.nextButton} onPress={() => {}}>
+      <RectButton style={styles.nextButton} onPress={handleCreateOrphanage}>
         <Text style={styles.nextButtonText}>Save</Text>
       </RectButton>
     </ScrollView>
@@ -93,6 +166,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     marginBottom: 16,
     textAlignVertical: 'top',
+  },
+
+  uploadImagesContainer: {
+    flexDirection: 'row'
+  },
+
+  uploadImage: {
+    width: 64,
+    height: 64,
+    borderRadius: 20,
+    marginBottom: 32,
+    marginRight: 8,
   },
 
   imagesInput: {
